93976b04cd29a9557ff35bda87a9da3732a9c18e
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-09-29 18:16:55 +0800

    Class1
    内容：let，print，字符串链接

---
+++ XcodeSudy1.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  HelloOSX
+//
+//  Created by 马颖捷 on 2017/9/29.
+//  Copyright © 2017年 Yuki.Ma. All rights reserved.
+//
+
+import Foundation
+
+var a=1
+var b=2
+let c=a+b
+//if use let, data cannot be changed
+print(c)
+
+var str = "Hello"
+str=str+",Yingjie"
+print(str)
+
+
