e02eea529574e4cfe6c9551864c097e604e1ea4b
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-09-30 12:20:36 +0800

    分支
    条件没有()
    禁止不适用{}

---
+++ XcodeStudy3.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HelloIOS01
+//
+//  Created by 马颖捷 on 2017/9/30.
+//  Copyright © 2017年 Yuki.Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        demo()
+        
+    }
+
+    func demo(){
+        let x = 10
+        if x>5 {
+            print("more then")
+        }else{print("less then")}
+        
+    }
+    
+    }
+
+
