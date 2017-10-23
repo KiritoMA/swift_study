917cf220424b5a8ef8424f4d8461d6680aa0a93f
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-10-23 23:05:10 +0800

    CALayer1

---
+++ CALayer1.swift
@@ -0,0 +1,16 @@
+import UIKit
+class ViewController:UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //240,240为正圆
+        let view=RoundView(frame: CGRect(x: 40, y: 40, width: 240, height: 140))
+        view.backgroundColor=UIBarButtonItem
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
