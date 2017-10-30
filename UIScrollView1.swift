6f0c2e1186f3dbb99c7c9cefc8deede9d8526c52
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-10-30 23:14:58 +0800

    FirstViewController

---
+++ UIScrollView1.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class SecondViewController : UIViewController{
+    override func viewDidLoad() {
+        self.view.backgroundColor=UIColor.brown
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
