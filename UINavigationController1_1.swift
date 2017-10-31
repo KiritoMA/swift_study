2a42e7a04962b3113d71c1cc97c56cd6e1c87788
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-10-31 15:41:35 +0800

    FirstViewController

---
+++ UINavigationController1_1.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title="第一页"
+        self.view.backgroundColor=UIColor.brown
+        //导航按钮
+        self.navigationItem.rightBarButtonItem=UIBarButtonItem(title: "下一页", style: UIBarButtonItemStyle.plain, target: self, action: #selector(FirstViewController.nextPage))
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @objc func nextPage()
+    {
+        let viewController = SecondViewController()
+        self.navigationController?.pushViewController(viewController, animated: true)
+        
+    }
+    
+}
