c16dcada9a66886df1173238a0564771678a39a9
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-10-31 15:42:41 +0800

    SecondViewController

---
+++ UINavigationController1_2.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    self.title="第二页"
+    self.view.backgroundColor=UIColor.purple
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
