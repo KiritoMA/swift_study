42faec81a385108f0adb1543e7606bac91c896cc
Yuki_MA <32018254+YingjieMA@users.noreply.github.com>
2017-11-29 16:27:02 +0800

    TestUIviewInPlayground

---
+++ TestUIview.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let view = UIView(frame: CGRect(x:3, y :0 , width: 200, height:200))
+view.backgroundColor = UIColor.brown
+view.layer.cornerRadius = 30
+view.layer.borderWidth = 10
+view.layer.borderColor = UIColor.blue.cgColor
+view
